7f3b0f246a60aa333bb75c95c23bb3e9b54f794e
Diehl, Paul <Paul.Diehl@coxautoinc.com>
2016-11-02 13:30:07 -0400

    adjusted for miniproject

--- js/index.js
+++ js/index.js
@@ -2,25 +2,32 @@ var app = angular.module('app', ['ngMaterial','angularGrid']);
 
 app.controller('MainCtrl', function($scope, $http, $q) {
   var vm = this;
+  var limit = 15;
   $scope.card = {};
-  $scope.card.title = 'test';
-  vm.page = 0;
+  vm.startPage = 1;
   vm.shots = [];
   vm.loadingMore = false;
 
   vm.loadMoreShots = function() {
 
-    if(vm.loadingMore) return;
-    vm.page++;
-    // var deferred = $q.defer();
+    if(vm.loadingMore || !vm.startPage) return;
+	
     vm.loadingMore = true;
-    var promise = $http.get('https://api.dribbble.com/v1/shots/?per_page=24&page='+vm.page+'&access_token=3df6bcfc60b54b131ac04f132af615e60b0bd0b1cadca89a4761cd5d125d608f');
+    var promise = $http.get('https://ej82jnn016.execute-api.us-east-1.amazonaws.com/dev/get-DDB-data' + "/" + limit + "/" + vm.startPage);
     promise.then(function(data) {
-
+      var payload = data.data
+	  var itemData = payload.hasOwnProperty('Item')  ? payload.Item :  payload.hasOwnProperty('Items') ? payload.Items : payload; 
+	  
       var shotsTmp = angular.copy(vm.shots);
-      shotsTmp = shotsTmp.concat(data.data);
+      shotsTmp = shotsTmp.concat(itemData);
       vm.shots = shotsTmp;
-      vm.loadingMore = false;
+	  vm.loadingMore = false;
+	  
+	  if (payload.hasOwnProperty('LastEvaluatedKey')) {
+      	vm.startPage = payload.LastEvaluatedKey.id;
+	  } else {
+	  	vm.startPage = false;
+	  }
 
     }, function() {
       vm.loadingMore = false;
